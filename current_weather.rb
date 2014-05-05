4c39b995dc3d3507ad89c16abb183c3b224e9d5b
rachellogie <rachel.logie1@gmail.com>
2014-05-05 11:51:28 -0600

    User can see current temp for london

---
+++ current_weather.rb
@@ -0,0 +1,14 @@
+require 'json'
+require 'faraday'
+
+response = Faraday.get 'http://api.openweathermap.org/data/2.5/weather?q=London,uk'
+
+json_data = response.body
+
+weather_description = JSON.parse(json_data)["weather"].first["description"]
+
+temperature = JSON.parse(json_data)["main"]["temp"].to_i
+#convert to fahrenheit from kelvin
+temperature = (1.8 * (temperature - 273.15) + 32).round(2)
+
+puts "London is #{temperature} degrees farenheit and #{weather_description}"
